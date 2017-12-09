4fa10f4a5f92ec7c5d0e72365cfccb681b8d1a78
Baron Roberts <baron@cthing.com>
2017-12-08 22:50:54 -0800

    Upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,9 +56,9 @@ group = "org.cthing"
 description = "A simple yet highly configurable XML writing library."
 
 dependencies {
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.0.1")
-    testCompile("org.junit.jupiter:junit-jupiter-params:5.0.1")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.0.1")
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.0.2")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.0.2")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.0.2")
     testCompileOnly("org.apiguardian:apiguardian-api:1.0.0")
     testCompile("org.assertj:assertj-core:3.8.0")
 
