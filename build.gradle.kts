1a9a87b5a976c4045797c963a18d517d05f311fd
Baron Roberts <baron@cthing.com>
2018-04-19 00:53:24 -0700

    Upgrade Gradle and other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,13 +51,13 @@ group = "org.cthing"
 description = "A simple yet highly configurable XML writing library."
 
 dependencies {
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.1.0")
-    testCompile("org.junit.jupiter:junit-jupiter-params:5.1.0")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.1.0")
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.1.1")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.1.1")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.1.1")
     testCompileOnly("org.apiguardian:apiguardian-api:1.0.0")
     testCompile("org.assertj:assertj-core:3.9.1")
 
-    spotbugsPlugins("com.mebigfatguy.fb-contrib:fb-contrib:7.2.0.sb")
+    spotbugsPlugins("com.mebigfatguy.fb-contrib:fb-contrib:7.2.1.sb")
 }
 
 tasks.withType<JavaCompile> {
@@ -83,7 +83,7 @@ tasks.withType<Javadoc> {
 }
 
 checkstyle {
-    toolVersion = "8.8"
+    toolVersion = "8.9"
     isIgnoreFailures = false
     configFile = project.file("dev/checkstyle/checkstyle.xml")
     configDir = project.file("dev/checkstyle")
@@ -107,7 +107,7 @@ tasks.withType<SpotBugsTask> {
 }
 
 jacoco {
-    toolVersion = "0.8.0"
+    toolVersion = "0.8.1"
 }
 
 (tasks["jacocoTestReport"] as JacocoReport).apply {
