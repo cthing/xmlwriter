5a396836ae631912c7ddef08598b94bf742b40aa
Baron Roberts <baron@cthing.com>
2018-09-19 20:19:40 -0700

    Upgrade Gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,11 @@ group = property("cthing.group") as String
 description = property("cthing.description") as String
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
     testImplementation("org.assertj:assertj-core:3.11.1")
     testCompileOnly("org.apiguardian:apiguardian-api:1.0.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
 
     spotbugsPlugins("com.mebigfatguy.fb-contrib:fb-contrib:7.4.3.sb")
 }
@@ -43,7 +43,7 @@ checkstyle {
 }
 
 spotbugs {
-    toolVersion = "3.1.6"
+    toolVersion = "3.1.7"
     isIgnoreFailures = false
     effort = "max"
     reportLevel = "medium"
