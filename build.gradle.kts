7e4e3386515c2f7f50732a63e0e0959edb5e65b9
Baron Roberts <baron@cthing.com>
2023-09-18 10:35:53 -0700

    Fix javadoc tasks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -140,7 +140,7 @@ val sourceJar by tasks.registering(Jar::class) {
 }
 
 val javadocJar by tasks.registering(Jar::class) {
-    from("javadoc")
+    from(tasks.getByName("javadoc"))
     archiveClassifier = "javadoc"
 }
 
@@ -149,8 +149,8 @@ publishing {
         register("jar", MavenPublication::class) {
             from(components["java"])
 
-            artifact(sourceJar.get())
-            artifact(javadocJar.get())
+            artifact(sourceJar)
+            artifact(javadocJar)
 
             pom {
                 name = project.name
